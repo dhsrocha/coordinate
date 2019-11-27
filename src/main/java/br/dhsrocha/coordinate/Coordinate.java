52016a81d6e6109cc0a67dbd291e903dc4744253
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2019-11-27 01:55:35 -0300

    Created the first factory method.

--- src/main/java/br/dhsrocha/coordinate/Coordinate.java
+++ src/main/java/br/dhsrocha/coordinate/Coordinate.java
@@ -1,9 +1,20 @@
 package br.dhsrocha.coordinate;
 
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
+import lombok.Value;
+
 /**
  * @author <a href="mailto:dhsrocha@gmail.com">Diego Rocha</a>
  * @since 1.0.0
  */
-final class Coordinate {
+@Value
+@NonNull
+@ToString
+@AllArgsConstructor(staticName = "of")
+public final class Coordinate {
 
+  double latitude;
+  double longitude;
 }
--- src/test/java/br/dhsrocha/coordinate/test/CoordinateTest.java
+++ src/test/java/br/dhsrocha/coordinate/test/CoordinateTest.java
@@ -1,9 +1,35 @@
 package br.dhsrocha.coordinate.test;
 
+import br.dhsrocha.coordinate.Coordinate;
+import lombok.val;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
 /**
  * @author <a href="mailto:dhsrocha@gmail.com">Diego Rocha</a>
  * @since 1.0.0
  */
+@DisplayName("Coordinate test case suite.")
 final class CoordinateTest {
 
+  private static final Duration TIMEOUT_LIMIT = Duration.ofMillis(20);
+
+  @Test
+  @DisplayName("Should create a coordinate before 20 milliseconds.")
+  final void shouldCreate_aCoordinate_before_20Millis() {
+    // Assert
+    assertTimeout(TIMEOUT_LIMIT, () -> {
+      // Act
+      val coordinate = Coordinate.of(45, 45);
+      // Assert
+      assertEquals(45, coordinate.getLatitude());
+
+      assertEquals(45, coordinate.getLongitude());
+    });
+  }
 }
